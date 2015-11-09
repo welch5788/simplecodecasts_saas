faebaea8eace0099abb80b796e4651ebb2a31685
morae88 <welch5788@gmail.com>
2015-11-09 16:42:53 +0000

    fixed files

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  has_one :profile
+  attr_accessor :stripe_card_token
   
   def save_with_payment
     if valid?
-      customer = StripeCustomer.create(description: email, plan: plan_id, card: stripe_card_token)
-      self.stripe_card_token = customer.id
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
       save!
     end
   end
